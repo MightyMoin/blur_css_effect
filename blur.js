625a8f89457451830b1305f14d64e6c58e8e6962
Moin Khan Pathan <54594146+MightyMoin@users.noreply.github.com>
2020-05-07 22:37:02 +0530

    final

---
+++ blur.js
@@ -0,0 +1,8 @@
+let box =document.querySelector('.box')
+box.addEventListener('mouseover', ()=>{
+    document.querySelector('.ori').classList.toggle('blu');
+})
+box.addEventListener('mouseout', ()=>{
+    document.querySelector('.ori').classList.toggle('blu');
+})
+
